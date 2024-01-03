b7a06ce91f120498064aefbeabcd98ab4da7e103
Katte <417560450@qq.com>
2024-01-04 00:09:43 +0800

    Update twikoo.js

--- netlify/functions/twikoo.js
+++ netlify/functions/twikoo.js
@@ -1,19 +1,20 @@
 const handler = require("twikoo-netlify").handler;
 
-exports.handler = async function (...args) {
-  // 获取event
-  const event = args[0];
-  const headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Content-Type",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-  };
-  if (event.httpMethod === "OPTIONS") {
-    return { statusCode: 200, headers };
-  } else {
-    // 调用twikoo-netlify的handler函数
-    const result = await handler(...args);
-    result.headers = { ...result.headers, ...headers };
-    return result;
-  }
-};
+exports.handler = handler;
+// exports.handler = async function (...args) {
+//   // 获取event
+//   const event = args[0];
+//   const headers = {
+//     "Access-Control-Allow-Origin": "*",
+//     "Access-Control-Allow-Headers": "Content-Type",
+//     "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+//   };
+//   if (event.httpMethod === "OPTIONS") {
+//     return { statusCode: 200, headers };
+//   } else {
+//     // 调用twikoo-netlify的handler函数
+//     const result = await handler(...args);
+//     result.headers = { ...result.headers, ...headers };
+//     return result;
+//   }
+// };
